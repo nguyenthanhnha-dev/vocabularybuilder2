b4111877c92f0d2c338a29c5ce3a542fde6b5185
Nha Nguyen <nha.nguyen@jiohealth.com>
2018-04-30 13:38:19 +0700

    Init Project

---
+++ src/main/java/com/nguyenthanhnha/vocabularybuilder/VocabularybuilderApplication.java
@@ -0,0 +1,12 @@
+package com.nguyenthanhnha.vocabularybuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabularybuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabularybuilderApplication.class, args);
+	}
+}
